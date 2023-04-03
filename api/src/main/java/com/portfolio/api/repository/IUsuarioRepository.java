c9772725a582b912b0cae25f66cda3c8cf3301b7
agustindiaz59 <matutias10000@gmail.com>
2023-04-03 19:20:04 -0300

    Error en BD

--- api/src/main/java/com/portfolio/api/repository/IUsuarioRepository.java
+++ api/src/main/java/com/portfolio/api/repository/IUsuarioRepository.java
@@ -4,5 +4,6 @@ import com.portfolio.api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario,Long> {
 }
--- api/src/main/java/com/portfolio/api/repository/ProyectoRepository.java
+++ api/src/main/java/com/portfolio/api/repository/ProyectoRepository.java
@@ -4,5 +4,6 @@ import com.portfolio.api.model.Proyecto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProyectoRepository extends JpaRepository<Proyecto,Long> {
 }
